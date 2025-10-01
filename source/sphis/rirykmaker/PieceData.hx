79d955f9625ccb414d4f2f4fdcb8ef91e9f2f9d9
Viwu <sinconsistencia@gmail.com>
2025-10-01 16:50:35 -0500

    PieceData class vars

---
+++ source/sphis/rirykmaker/PieceData.hx
@@ -0,0 +1,11 @@
+package sphis.rirykmaker;
+
+class PieceData
+{
+	public static var pieceSizes:Map<String, Int> = [];
+
+	public static var pieceMin:Map<String, Int> = [];
+	public static var pieceMax:Map<String, Int> = [];
+
+	public static var pieceTypes:Map<String, String> = [];
+}
