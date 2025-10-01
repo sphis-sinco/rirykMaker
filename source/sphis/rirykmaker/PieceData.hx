e2a1d4d41a929701634c55f76a16d4e66d5887c6
Viwu <sinconsistencia@gmail.com>
2025-10-01 16:59:49 -0500

    pieceTypes map val fix + implementation of getPieceInfo

--- source/sphis/rirykmaker/PieceData.hx
+++ source/sphis/rirykmaker/PieceData.hx
@@ -9,7 +9,7 @@ class PieceData
 	public static final pieceMin:Map<String, Int> = [];
 	public static final pieceMax:Map<String, Int> = [];
 
-	public static final pieceTypes:Map<String, String> = [];
+	public static final pieceTypes:Map<String, Array<String>> = [];
 
 	public static function getPieceInfo(piece:String)
 	{
@@ -20,8 +20,29 @@ class PieceData
 		final minMaxPath = pathPrefix + piece + '-min-max' + pathSuffix;
 		final typesPath = pathPrefix + piece + '-types' + pathSuffix;
 
-		if (Assets.exists(sizesPath)) {}
-		if (Assets.exists(minMaxPath)) {}
-		if (Assets.exists(typesPath)) {}
+		if (Assets.exists(sizesPath))
+		{
+			final sizes = Assets.getText(sizesPath).split('\n');
+			pieceSizes.set(piece, sizes);
+
+			trace('Riryk piece: $piece sizes: $sizes');
+		}
+
+		if (Assets.exists(minMaxPath))
+		{
+			final minMax = Assets.getText(minMaxPath).split('\n');
+			pieceMin.set(piece, Std.parseInt(minMax[0]));
+			pieceMax.set(piece, Std.parseInt(minMax[1]));
+
+			trace('Riryk piece: $piece min-max values: $minMax');
+		}
+
+		if (Assets.exists(typesPath))
+		{
+			final types = Assets.getText(typesPath).split('\n');
+			pieceTypes.set(piece, types);
+
+			trace('Riryk piece: $piece types: $types');
+		}
 	}
 }
