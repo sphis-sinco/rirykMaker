bdd56dc62f3b82eb1d1ba28db2c2243c53eeda28
Viwu <sinconsistencia@gmail.com>
2025-10-01 19:36:33 -0500

    bg

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,19 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var bg:FlxSprite;
+
 	override public function create()
 	{
 		super.create();
+
+		bg.makeGraphic(FlxG.width, FlxG.height);
+		add(bg);
 	}
 
 	override public function update(elapsed:Float)
